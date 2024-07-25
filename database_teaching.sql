9aed0326eafce240fdc21cb6e3773566d703e6f9
Tori Robinson <156717220+ToriiX@users.noreply.github.com>
2024-07-25 15:06:08 +0300

    Update database_teaching.sql

--- database_teaching.sql
+++ database_teaching.sql
@@ -1,19 +1,22 @@
-CREATE DATABASE "no_students"
+-- Create database
+CREATE DATABASE "language_students"
 
+
+-- Create learning table
 CREATE TABLE student_info (
     student_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    phone_number INTEGER,
-    country_from VARCHAR(20),
-    country_living VARCHAR(20),
-    start_month_year DATE,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR, 
+    gender VARCHAR(1),
+    country_from VARCHAR,
+    country_living VARCHAR,
+    start_date  DATE,
     student_level_start VARCHAR(2),
-    level_comment VARCHAR(50),
+    level_comment VARCHAR(20),
     adult BOOLEAN,
     active BOOLEAN,
     price_per_class NUMERIC,
-    end_month_year DATE
+    end_date DATE
 );
 
 
@@ -43,7 +46,7 @@ CREATE TABLE study_plan (
 -- Create material table
 CREATE TABLE material (
     student_level VARCHAR(2),
-	material_type VARCHAR(20)
+    material_type VARCHAR(20),
     nouns VARCHAR(100),
     adjectives VARCHAR(100),
     adverbs VARCHAR(100),
@@ -52,5 +55,8 @@ CREATE TABLE material (
     vocabulary VARCHAR(100),
     pronunciation VARCHAR(100),
     other VARCHAR(100),
-	note VARCHAR(100)
+    note VARCHAR(100)
 );
+
+
+
